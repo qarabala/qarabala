9781c08b24db7c5a6682ff13e3e12777072186ee
qarabala <turalnagi@gmail.com>
2022-07-13 09:51:44 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from requests import get
+from bs4 import BeautifulSoup as BS
+import requests 
+import csv
+from alive_progress import alive_bar
+
+n=0
+f= open("qiymetler.csv", "w",encoding="utf-8",newline='')
+writer = csv.writer(f)
+
+with alive_bar(500) as bar:
+    for mehsul_novu in ['meyve-terevez-quru-meyveler/meyve','meyve-terevez-quru-meyveler/goyerti','meyve-terevez-quru-meyveler/terevez','meyve-terevez-quru-meyveler/quru-meyve',
+                        'et-toyuq-deniz-mehsullari/mal%20eti','et-toyuq-deniz-mehsullari/toyuq','et-toyuq-deniz-mehsullari/deniz-mehsullari',
+                        'qastronom/dondurma','qastronom/kolbasa-sosisler','qastronom/donmus-mehsullar']:
+        page=1
+        dovr=True
+
+        while dovr:
+            url = 'https://neptun.az/'+mehsul_novu+'?page='+str(page)
+            result=requests.get(url)
+            soup=BS(result.text,'html.parser')
+            print(url+"     |")
+
+            if(len(soup.find_all("div", class_="caption"))):
+                for tags in soup.find_all("div", class_="caption"):
+                    try:
+                        n+=1
+                        row=str(n), tags.find("h4").text, tags.find("span",class_="price-new").string, mehsul_novu
+                        writer.writerow(row)
+                    except:
+                        print(row+(' yaza bilmedi',))    
+                    bar()
+                page+=1 
+            else:
+                dovr=False
+print(str(n)+" sayda mehsul yazildi!")
+f.close
