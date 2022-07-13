a69753341145ae3985f4704c1dd3669093c24b5d
qarabala <turalnagi@gmail.com>
2022-07-13 09:54:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from requests import get
+#Scraping product prices from web in Azerbaijan marketplace
+
 from bs4 import BeautifulSoup as BS
 import requests 
 import csv
 from alive_progress import alive_bar
 
 n=0
-f= open("qiymetler.csv", "w",encoding="utf-8",newline='')
+f= open("qiymetler.csv", "w",encoding="utf-8",newline='')  #All result will record to new csv file
 writer = csv.writer(f)
 
+#This is parent categories of products
 with alive_bar(500) as bar:
     for mehsul_novu in ['meyve-terevez-quru-meyveler/meyve','meyve-terevez-quru-meyveler/goyerti','meyve-terevez-quru-meyveler/terevez','meyve-terevez-quru-meyveler/quru-meyve',
                         'et-toyuq-deniz-mehsullari/mal%20eti','et-toyuq-deniz-mehsullari/toyuq','et-toyuq-deniz-mehsullari/deniz-mehsullari',
@@ -33,5 +35,5 @@ with alive_bar(500) as bar:
                 page+=1 
             else:
                 dovr=False
-print(str(n)+" sayda mehsul yazildi!")
+print(str(n)+" sayda mehsul yazildi!")  #Little report after execution
 f.close
